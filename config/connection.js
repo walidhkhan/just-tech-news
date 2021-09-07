a1970e80bb309b04cbfd7983eb763f9119fac68c
walidhkhan <walidhkhan@gmail.com>
2021-09-07 13:02:04 -0700

    completed module section 14.1.5

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,8 @@ if (process.env.JAWSDB_URL) {
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
+    user: 'root',
+    password: 'Bangladesh1997!',
     dialect: 'mysql',
     port: 3306
   });
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
@@ -15,3 +20,6 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
